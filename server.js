cde2d9da6611f465dadf72b91b18c4009c55cc80
joesch21 <joesch@hotmail.com>
2024-11-13 20:20:59 +1100

    package.json refinedment

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const path = require('path'); // Import the path module
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Root route can serve a test message
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-
-// Serve static files from the React build directory
-app.use(express.static('build'));
 
 // For any other routes, serve the React app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
